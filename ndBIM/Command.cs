ae5d0677ddde188960a8be6977aced5be1b73c54
Deyan Nenov <Deyan Nenov>
2017-03-04 13:09:37 +0000

    form.1

--- ndBIM/Command.cs
+++ ndBIM/Command.cs
@@ -24,24 +24,30 @@ namespace ndBIM
             Application app = uiapp.Application;
             Document doc = uidoc.Document;
 
+            using (ndBIMInterface m_form = new ndBIMInterface(uiapp))
+            {
+                var result = m_form.Show();
+            }
+
+
             // Access current selection
 
-            Selection sel = uidoc.Selection;
+            //Selection sel = uidoc.Selection;
 
             // Retrieve elements from database
 
-            FilteredElementCollector col
-              = new FilteredElementCollector(doc)
-                .WhereElementIsNotElementType()
-                .OfCategory(BuiltInCategory.INVALID)
-                .OfClass(typeof(Wall));
+            //FilteredElementCollector col
+            //  = new FilteredElementCollector(doc)
+            //    .WhereElementIsNotElementType()
+            //    .OfCategory(BuiltInCategory.INVALID)
+            //    .OfClass(typeof(Wall));
 
             // Filtered element collector is iterable
 
-            foreach (Element e in col)
-            {
-                Debug.Print(e.Name);
-            }
+            //foreach (Element e in col)
+            //{
+            //    Debug.Print(e.Name);
+            //}
 
             // Modify document within a transaction
 
---
+++ ndBIM/ndBIMInterface.cs
@@ -0,0 +1,33 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ndBIM
+{
+    public partial class ndBIMInterface : Form
+    {
+        private UIApplication uiapp;
+        private UIDocument uidoc;
+        private Autodesk.Revit.DB.Document doc;
+
+        public ndBIMInterface(UIApplication uiapp)
+        {
+            InitializeComponent();
+
+            this.uiapp = uiapp;
+            this.uidoc = uiapp.ActiveUIDocument;
+            this.doc = uidoc.Document;
+            if (doc == null)
+            {
+                return;
+            }
+        }
+    }
+}
